23a8a1bf6d9919aa3c782e404568b954697a363d
Matt Whitaker <matt.whitaker@concur.com>
2016-10-18 22:27:56 -0700

    changing how images are handled

--- gulpfile.js
+++ gulpfile.js
@@ -22,24 +22,18 @@ const EJS_GLOB = `${SRC_ROOT}**/*.ejs`;
 const LESS_RESET_GLOB = `${SRC_ROOT}_3rd-party/reset.less`;
 const LESS_GLOB = `${SRC_ROOT}**/*.less`;
 const LESS_EXCL_GLOB = `!${SRC_ROOT}_3rd-party/font-awesome/**/*.less`;
-const IMAGE_GLOB = 'images/**/*';
-const YML_GLOB = `${SRC_ROOT}**/*.yml`;
 
 const DEST_ROOT = 'themes/dt/';
 const DEST_LAYOUT_PATH = `${DEST_ROOT}layout/`;
-const DEST_YML_PATH = `${DEST_ROOT}`;
 const DEST_CSS_PATH = `${DEST_ROOT}source/css/`;
 const DEST_CSS_FILENAME = 'app.css';
-const DEST_IMAGE_PATH = `${DEST_ROOT}source/images/`;
 
 const PUBLIC_ROOT = 'public/';
-
-const FONT_EXTENSIONS = ['otf', 'eot', 'svg', 'ttf', 'woff', 'woff2'];
 
 const hexo = new Hexo(process.cwd(), {});
 
 gulp.task('clean:assets', () => {
-  return del([ './source', './images' ]);
+  return del([ './source' ]);
 });
 
 if (process.env.NODE_ENV === 'production') {
@@ -72,16 +66,6 @@ gulp.task('copy:ejs', () => {
     .pipe(gulp.dest(DEST_LAYOUT_PATH));
 });
 
-gulp.task('copy:yml', () => {
-  return gulp.src(YML_GLOB)
-    .pipe(gulp.dest(DEST_YML_PATH));
-});
-
-gulp.task('copy:images', [], () => {
-  return gulp.src(IMAGE_GLOB)
-    .pipe(gulp.dest(DEST_IMAGE_PATH));
-});
-
 gulp.task('compile:less', () => {
   return gulp.src([LESS_RESET_GLOB, LESS_GLOB, LESS_EXCL_GLOB])
     .pipe(less(LESS_GLOB))
@@ -106,15 +90,11 @@ gulp.task('clean:public', () => {
 gulp.task('clean', ['clean:theme', 'clean:public']);
 
 gulp.task('build', [
-  'copy:images', 
-  'copy:yml', 
   'copy:ejs', 
   'compile:less'
 ]);
 
 gulp.task('build:watch', [
-  'copy:images',
-  'copy:yml',
   'copy:ejs',
   'compile:less',
   'retrieve:assets'
