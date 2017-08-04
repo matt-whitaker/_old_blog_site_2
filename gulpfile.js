b0ea1cc7b01f88eabd50b320f90c3724fa38dca0
Matt Whitaker <matt.whitaker@concur.com>
2017-08-03 20:37:35 -0700

    hmm

--- gulpfile.js
+++ gulpfile.js
@@ -21,12 +21,9 @@ require('dotenv').config();
 const download = require('./utils/download.js');
 
 const SRC_ROOT = 'src/';
-const EJS_GLOB = `${SRC_ROOT}**/*.ejs`;
+const DEST_ROOT = 'themes/dt/';
 const LESS_RESET_GLOB = `${SRC_ROOT}less/_3rd-party/reset.less`;
 const LESS_GLOB = `${SRC_ROOT}less/**/*.less`;
-
-const DEST_ROOT = 'themes/dt/';
-const DEST_LAYOUT_PATH = `${DEST_ROOT}layout/`;
 const DEST_CSS_PATH = `${DEST_ROOT}source/css/`;
 const DEST_CSS_FILENAME = 'app.css';
 
@@ -34,41 +31,7 @@ const PUBLIC_ROOT = 'public/';
 
 const hexo = new Hexo(process.cwd(), {});
 
-gulp.task('clean:assets', () => {
-  return del([ './source' ]);
-});
-
-// if (process.env.NODE_ENV === 'production') {
-//   gulp.task('retrieve:assets', ['clean:assets'], () => {
-//     return download();
-//   });
-// } else {
-//   // copy the assets from the depth of the machine... ie my dropbox folder
-//   gulp.task('retrieve:assets', ['clean:assets'], () => {
-//     const copyAssets = () => (new Promise((res, rej) => {
-//       gulp.src(['**/*'], { cwd: process.env.DROPBOX_PATH })
-//         .pipe(gulp.dest('./'))
-//         .on('end', res)
-//         .on('error', (err) => console.error(err) || rej(err));
-//     }));
-//
-//     const moveConfig = () => (new Promise((res, rej) => {
-//       gulp.src('config.')
-//         .pipe(gulp.dest('./'))
-//         .on('end', res)
-//         .on('error', (err) => console.error(err) || rej(err));
-//     }));
-//
-//     return copyAssets().then(moveConfig);
-//   });
-// }
-
-gulp.task('copy:ejs', () => {
-  return gulp.src(EJS_GLOB)
-    .pipe(gulp.dest(DEST_LAYOUT_PATH));
-});
-
-gulp.task('compile:less', () => {
+gulp.task('less', () => {
   return gulp.src([LESS_RESET_GLOB, LESS_GLOB])
     .pipe(less(LESS_GLOB))
     .pipe(sourcemaps.init())
@@ -77,33 +40,9 @@ gulp.task('compile:less', () => {
     .pipe(gulp.dest(DEST_CSS_PATH));
 });
 
-gulp.task('clean:theme', () => {
-  return del([
-    DEST_ROOT
-  ]);
-});
-
-gulp.task('clean:public', () => {
-  return del([
-    PUBLIC_ROOT
-  ]);
-});
-
-gulp.task('clean', ['clean:theme', 'clean:public']);
-
-gulp.task('build', [
-  'copy:ejs', 
-  'compile:less'
-]);
-
-gulp.task('build:watch', [
-  'copy:ejs',
-  'compile:less',
-  'retrieve:assets'
-]);
-
 const config = {
   src: PUBLIC_ROOT,
 };
 
 gulp.task('deploy', deploy(config));
+gulp.task('build', ['less']);
